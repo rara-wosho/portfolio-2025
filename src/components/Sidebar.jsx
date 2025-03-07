5ad587862f164b98b678a6a2d316090cbdd6ebe2
Rara <deverinth88@gmail.com>
2025-03-07 17:50:44 +0800

    updated sidebar content

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,14 @@ import React from "react";
 const Sidebar = ({ setSidebar }) => {
   return (
     <div onClick={() => setSidebar((s) => !s)} className="backdrop--sidebar">
-      <div className="--sidebar h-100 glass-card p-3 shadow">
+      <div className="--sidebar overflow-hidden h-100 bg-dark p-3 shadow">
         <div className="d-flex align-items-center">
+          <img
+            style={{ width: 16, height: 16 }}
+            src="/logo/rd_logo.png"
+            alt=""
+          />
+          <p className="mb-0 ms-1 fw-vold text-secondary">Rael</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -15,19 +21,17 @@ const Sidebar = ({ setSidebar }) => {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="lucide lucide-arrow-right-to-line me-2 pointer"
+            className="lucide lucide-arrow-right-to-line ms-auto pointer"
           >
             <path d="M17 12H3" />
             <path d="m11 18 6-6-6-6" />
             <path d="M21 5v14" />
           </svg>
-          <img
-            style={{ width: 16, height: 16 }}
-            src="/logo/rd_logo.png"
-            alt=""
-          />
-          <p className="mb-0 ms-1 text-secondary">Rael</p>
         </div>
+
+        <p className="mb-0 text-secondary fs-7 mt-3 text-nowrap">
+          Contents are currently not available
+        </p>
       </div>
     </div>
   );
