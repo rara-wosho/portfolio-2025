eb4fbbb6daa2337f34265e9e6a6c88ad79a98620
Rara <deverinth88@gmail.com>
2025-03-08 17:46:59 +0800

    minor change to glowing btn

--- src/components/ui/GlowingBtn.jsx
+++ src/components/ui/GlowingBtn.jsx
@@ -4,9 +4,12 @@ const GlowingBtn = ({ label, icon }) => {
   return (
     <div className="glowing-primary-btn-container overflow-hidden position-relative rounded-3">
       <div className="glowing-border"></div>
-      <div className="glowing-primary-btn pointer px-3 rounded-3 py-2 d-flex align-items-center">
+      <div className="glowing-primary-btn pointer px-2 px-md-3 rounded-3 py-1 py-md-2 d-flex align-items-center">
         {icon && icon}
-        <p style={{ color: "rgb(248,248,248)" }} className="mb-0 fw-bold fs-7">
+        <p
+          style={{ color: "rgb(248,248,248)" }}
+          className="mb-0 fw-semibold fs-7"
+        >
           {label}
         </p>
       </div>
